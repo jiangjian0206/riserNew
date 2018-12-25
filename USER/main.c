da5f30fce97925f7b08e7b0a6e3dd023ccea96fb
jiangjian0206 <756028992@qq.com>
2018-12-25 17:53:44 +0800

    remove 4k rom

--- USER/main.c
+++ USER/main.c
@@ -47,124 +47,7 @@ unsigned char BuffSprintf[10];
 int test_point1=54,test_point2=54;
 int test_setPin =1;
 
-/*******************************************************************************
-* 函数名  : W5500_Initialization
-* 描述    : W5500初始货配置
-* 输入    : 无
-* 输出    : 无
-* 返回值  : 无
-* 说明    : 无
-*******************************************************************************/
-void W5500_Initialization(void)
-{
-	W5500_Init();		//初始化W5500寄存器函数
-	Detect_Gateway();	//检查网关服务器 
-	Socket_Init(0);		//指定Socket(0~7)初始化,初始化端口0
-}
 
-/*******************************************************************************
-* 函数名  : Load_Net_Parameters
-* 描述    : 装载网络参数
-* 输入    : 无
-* 输出    : 无
-* 返回值  : 无
-* 说明    : 网关、掩码、物理地址、本机IP地址、端口号、目的IP地址、目的端口号、端口工作模式
-*******************************************************************************/
-void Load_Net_Parameters(void)
-{
-	Gateway_IP[0] = 192;//加载网关参数
-	Gateway_IP[1] = 168;
-	Gateway_IP[2] = 1;
-	Gateway_IP[3] = 1;
-
-	Sub_Mask[0]=255;//加载子网掩码
-	Sub_Mask[1]=255;
-	Sub_Mask[2]=255;
-	Sub_Mask[3]=0;
-
-	Phy_Addr[0]=0x0c;//加载物理地址
-	Phy_Addr[1]=0x29;
-	Phy_Addr[2]=0xab;
-	Phy_Addr[3]=0x7c;
-	Phy_Addr[4]=0x00;
-	Phy_Addr[5]=0x01;
-
-	IP_Addr[0]=192;//加载本机IP地址
-	IP_Addr[1]=168;
-	IP_Addr[2]=1;
-	IP_Addr[3]=199;
-
-	S0_Port[0] = 0x13;//加载端口0的端口号5000 
-	S0_Port[1] = 0x88;
-
-	S0_DIP[0]=192;//加载端口0的目的IP地址
-	S0_DIP[1]=168;
-	S0_DIP[2]=1;
-	S0_DIP[3]=190;
-	
-	S0_DPort[0] = 0x17;//加载端口0的目的端口号6000
-	S0_DPort[1] = 0x70;
-
-	S0_Mode=TCP_CLIENT;//加载端口0的工作模式,TCP客户端模式
-}
-
-/*******************************************************************************
-* 函数名  : W5500_Socket_Set
-* 描述    : W5500端口初始化配置
-* 输入    : 无
-* 输出    : 无
-* 返回值  : 无
-* 说明    : 分别设置4个端口,根据端口工作模式,将端口置于TCP服务器、TCP客户端或UDP模式.
-*			从端口状态字节Socket_State可以判断端口的工作情况
-*******************************************************************************/
-void W5500_Socket_Set(void)
-{
-	if(S0_State==0)//端口0初始化配置
-	{
-		if(S0_Mode==TCP_SERVER)//TCP服务器模式 
-		{
-			if(Socket_Listen(0)==TRUE)
-				S0_State=S_INIT;
-			else
-				S0_State=0;
-		}
-		else if(S0_Mode==TCP_CLIENT)//TCP客户端模式 
-		{
-			if(Socket_Connect(0)==TRUE)
-				S0_State=S_INIT;
-			else
-				S0_State=0;
-		}
-		else//UDP模式 
-		{
-			if(Socket_UDP(0)==TRUE)
-				S0_State=S_INIT|S_CONN;
-			else
-				S0_State=0;
-		}
-	}
-}
-
-/*******************************************************************************
-* 函数名  : Process_Socket_Data
-* 描述    : W5500接收并发送接收到的数据
-* 输入    : s:端口号
-* 输出    : 无
-* 返回值  : 无
-* 说明    : 本过程先调用S_rx_process()从W5500的端口接收数据缓冲区读取数据,
-*			然后将读取的数据从Rx_Buffer拷贝到Temp_Buffer缓冲区进行处理。
-*			处理完毕，将数据从Temp_Buffer拷贝到Tx_Buffer缓冲区。调用S_tx_process()
-*			发送数据。
-*******************************************************************************/
-void Process_Socket_Data(SOCKET s)
-{
-	unsigned short size;
-	size=Read_SOCK_Data_Buffer(s, Rx_Buffer);
-	memcpy(Tx_Buffer, Rx_Buffer, size);			
-	Write_SOCK_Data_Buffer(s, Tx_Buffer, size);
-}
-//int strcpmSelf(char *point )
-//{
 
 //}
 void usart(void)
@@ -535,9 +418,9 @@ void System_Initialization(void)
 {
 	RCC_Configuration();		//设置系统时钟为72MHZ(这个可以根据需要改)
   	NVIC_Configuration();		//STM32中断向量表配配置
-	SPI_Configuration();		//W5500 SPI初始化配置(STM32 SPI1)
+
 	Timer2_Init_Config();		//Timer2初始化配置
-	W5500_GPIO_Configuration();	//W5500 GPIO初始化配置	
+	
 }
 
 /*******************************************************************************
